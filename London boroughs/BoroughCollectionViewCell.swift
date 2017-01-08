58778a72869dc51d578a098c3cc30e61732430fc
D.A Jalloh <info@codewithjalloh.com>
2017-01-08 18:19:54 +0000

    cells classes added

---
+++ London boroughs/BoroughCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BoroughCollectionViewCell.swift
+//  London boroughs
+//
+//  Created by wealthyjalloh on 08/01/2017.
+//  Copyright © 2017 CWJ. All rights reserved.
+//
+
+import UIKit
+
+class BoroughCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var boroughImage: UIImageView!
+    @IBOutlet weak var boroughTitle: UILabel!
+    
+}
---
+++ London boroughs/DetailsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsTableViewCell.swift
+//  London boroughs
+//
+//  Created by wealthyjalloh on 08/01/2017.
+//  Copyright © 2017 CWJ. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var linkLabel: UILabel!
+    
+    
+    
+}
