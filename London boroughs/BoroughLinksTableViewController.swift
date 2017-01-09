7f52bd235c31b713d983556fe02c46cec7b70bc8
D.A Jalloh <info@codewithjalloh.com>
2017-01-09 23:04:47 +0000

    BoroughLinksTableViewController added

--- London boroughs/BoroughLinksTableViewController.swift
+++ London boroughs/BoroughLinksTableViewController.swift
@@ -23,6 +23,12 @@ class BoroughLinksTableViewController: UITableViewController, SFSafariViewContro
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        headerImage.image = image
+        navigationItem.title = locationLinksTitle
+        
+        navigationController?.navigationBar.tintColor = .black
+        
+        
 
         
     }
@@ -34,69 +40,47 @@ class BoroughLinksTableViewController: UITableViewController, SFSafariViewContro
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return links.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LinkCell", for: indexPath) as! DetailsTableViewCell
+        
+        
+        let arrayOfLinks = links[indexPath.row]
+        let linkTitle = arrayOfLinks.keys
+        let linkTitleResult = Array(linkTitle)
+        
+        cell.linkLabel.text = linkTitleResult[linkTitleResult.count - 1]
 
         // Configure the cell...
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // access the value of each dictionary in the list array 
+        
+        let arrayOfLinks = links[indexPath.row]
+        let linkUrl = arrayOfLinks.values
+        var url = Array(linkUrl)[linkUrl.count - 1]
+        
+        if url.hasPrefix("http://") == false && url.hasPrefix("https://") == false {
+            url = "http://" + url
+        }
+        
+        let safariVC = SFSafariViewController(url: URL(string: url)!)
+        safariVC.view.tintColor = .black
+        safariVC.delegate = self
+        self.present(safariVC, animated: true, completion: nil)
+        
+        
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
