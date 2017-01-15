6d4b472a1e906b8ac3b60c427e4337d88d5aae18
D.A Jalloh <info@codewithjalloh.com>
2017-01-15 23:07:11 +0000

    update locations borough hounslow

--- London boroughs/LocationController.swift
+++ London boroughs/LocationController.swift
@@ -9,4 +9,6 @@
 import Foundation
 
 
-var locations = [barking, barnet, camden, croydon, greenwich,  hackney, hammersmith, islington, KensingtonAndChelsea, lambeth, lewisham, southwark, towerHamlets, wandsworth, westminister, sutton ]
+// update locations borough
+
+var locations = [barking, barnet, camden, croydon, greenwich,  hackney, hammersmith, islington, KensingtonAndChelsea, lambeth, lewisham, southwark, towerHamlets, wandsworth, westminister, sutton, hounslow ]
